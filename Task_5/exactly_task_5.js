46153d0b30ed2cda68e015aef14d59ccd0703c04
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2023-10-09 23:52:49 +0300

    a2

--- Task_5/exactly_task_5.js
+++ Task_5/exactly_task_5.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function isNumber(value) {
-        return /^[0-9]+$/.test(value);
-    }
-
+    var total;
+    var price;
     var calculateButton = document.getElementById("calculate");
-
     calculateButton.addEventListener("click", function () {
         var quantityInput = document.getElementById("quantity");
         var productSelect = document.getElementById("product");
         var result = document.getElementById("result");
-
         var quantity = parseInt(quantityInput.value);
         var product = productSelect.value;
-
-        if (!isNumber(quantityInput.value)) {
+        if (!(/^[0-9]+$/.test(quantityInput.value))) {
             result.innerHTML = "Ошибка: введите корректное количество товара.";
         } else {
-            var price;
             if (product === "product1") {
                 price = 100;
             } else if (product === "product2") {
@@ -24,8 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else if (product === "product3") {
                 price = 300;
             }
-
-            var total = quantity * price;
+            total = quantity * price;
             result.innerHTML = "Стоимость заказа: " + total;
         }
     });
